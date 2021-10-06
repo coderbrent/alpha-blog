eccdbb0eb700e54b9c646299b2872cf3d20000f5
Brent Abruzese <brent.abruzese@gmail.com>
2021-10-05 23:20:39 -0400

    init

---
+++ db/migrate/20210916032244_add_description_field_to_articles.rb
@@ -0,0 +1,5 @@
+class AddDescriptionFieldToArticles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :description, :string
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  # Character.create(name: 'Luke', movie: movies.first)
+
+  articles = Article.create(title: "Functional Programming in JavaScript", description: "Functional programming has become a very popular way to code as of lately.")
